f651657ea80207d57610fcd2e8ae37bcc41b10a7
Emiliano Alonzo <emialonzo@gmail.com>
2019-11-14 15:48:04 -0300

    Modify generator and add templates to generate classes instead of types. The generated code has errors

--- swaggerFiles/generator.ts
+++ swaggerFiles/generator.ts
@@ -12,26 +12,32 @@ let folders = [
 let templatePath = folder => `swaggerFiles/${folder}/swagger.yaml` 
 let substitutionString = (strName : String) => strName.replace(new RegExp("-", 'g'), "_")
 
-function generateCode(folder){
-    var file = templatePath(folder)
+function generateCode(name, file){
     var swagger = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
     // var swagger = JSON.parse(fs.readFileSync(file, "UTF-8"));
     var tsSourceCode = CodeGen.getTypescriptCode({
-      className: substitutionString(folder) ,
+      className: substitutionString(name) ,
       swagger: swagger,
-      imports: ["../../typings/tsd.d.ts"]
+      imports: ["../../typings/tsd.d.ts"],
+      template: {
+        class: fs.readFileSync("swaggerFiles/templates/class.mustache", "utf-8"),
+        // method: fs.readFileSync("my-method.mustache", "utf-8"),
+        type: fs.readFileSync("swaggerFiles/templates/type.mustache", "utf-8")
+      }
     });
 
     // Write data in 'Output.txt' . 
-    fs.writeFile(`swaggerFiles/generatedCode/${folder}.ts`, tsSourceCode, (err) => { 
+    fs.writeFile(`swaggerFiles/generatedCode/${name}.ts`, tsSourceCode, (err) => { 
         // In case of a error throw err. 
         if (err) throw err; 
     }) 
 }
 
 folders.forEach(folder => {
-    generateCode(folder)
+    generateCode(folder, templatePath(folder))
 });
+
+generateCode("modelos", 'swaggerFiles/utm-domains/utm-domain-commons.yaml')
 
 
 
