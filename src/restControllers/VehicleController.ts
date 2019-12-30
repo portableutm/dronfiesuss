2ad74ca677cdb76d593b69adf0ea0e56025da4c1
Emiliano Alonzo <emialonzo@gmail.com>
2019-12-30 14:45:17 -0300

    Update vehicle rest controller

--- src/restControllers/VehicleController.ts
+++ src/restControllers/VehicleController.ts
@@ -1,15 +1,21 @@
 import {NextFunction, Request, Response} from "express";
 import { VehicleDao } from "../daos/VehicleDao";
 
-export class UTMMessageController {
+export class VehicleController {
 
     private dao = new VehicleDao()
+    
+    // let x = {a: 1, b: 2, c: 3, z:26};
+    // let {b, ...y} = x;
+    // b ==2
+    // y == {a: 1, c: 3, z:26};
 
     async all(request: Request, response: Response, next: NextFunction) {
         return this.dao.all();
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
+        console.log(request.body)
         return this.dao.one(parseInt(request.params.id));
     }
 
