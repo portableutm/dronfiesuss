79f1431f1c37e76e70e713383d50ca77d92b98b9
Emiliano Alonzo <emialonzo@gmail.com>
2020-01-31 16:51:00 -0300

    Add method operationByCreator. All method its only bu admin. One method check if the user is pilot, then only can get the operation created by logged user

--- src/restControllers/OperationController.ts
+++ src/restControllers/OperationController.ts
@@ -1,79 +1,112 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import { OperationDao } from "../daos/OperationDaos";
 import { Role } from "../entities/User";
+import { getPayloadFromResponse } from "../utils/authUtils";
 
 export class OperationController {
 
-    private dao = new OperationDao()
+  private dao = new OperationDao()
 
+  /**
+   * Return all operations, if state passed return all operations with this state
+   * query params: state=OperationState 
+   * @param request 
+   * @param response 
+   * @param next 
+   */
+  //solo admin   
+  async all(request: Request, response: Response, next: NextFunction) {
+    let { role } = getPayloadFromResponse(response)
+    if (role == Role.ADMIN) {
+      let state = request.query.state
+      let ops = await this.dao.all({ state: state })
+      return response.json({ count: ops.length, ops })
+    }
+    else {
+      return response.sendStatus(401)
+    }
+
+  }
+
+
+  //si es admin cualquiera, si no es dueño o no existe por id 404   
+  async one(request: Request, response: Response, next: NextFunction) {
+    // console.log(` ---> request.params.gufi:${request.params.id}`)
+    try {
+      let { role, username } = getPayloadFromResponse(response)
+      if (role == Role.ADMIN) {
+        return response.json(await this.dao.one(request.params.id));
+      } else {
+        let v = await this.dao.oneByCreator(request.params.id, username);
+        return response.json(v)
+      }
+    } catch (error) {
+      return response.sendStatus(404)
+    }
+  }
+
+  /**
+   * invalid data: especificar cual esta mal > no guardo en BD
+   * 
+   * @param request 
+   * @param response 
+   * @param next 
+   */
+  async save(request: Request, response: Response, next: NextFunction) {
     /**
-     * Return all operations, if state passed return all operations with this state
-     * query params: state=OperationState 
-     * @param request 
-     * @param response 
-     * @param next 
+     * interesccion con otras operaciones
+     * interseccion con UVR
      */
-    async all(request: Request, response: Response, next: NextFunction) {
-      let state = request.query.state;
-      let ops = await this.dao.all({state:state})
-      return response.json({count:ops.length, ops});
-    }
-
-    async one(request: Request, response: Response, next: NextFunction) {
-      // console.log(` ---> request.params.gufi:${request.params.id}`)
-      return response.json(await this.dao.one(request.params.id));
-    }
-
-    async save(request: Request, response: Response, next: NextFunction) {
-      let op_vols = request.body.operation_volumes
-      let error = false
-      if(op_vols !== undefined){
-          for (let index = 0; index < op_vols.length; index++) {
-            const element = op_vols[index];
-            let intersect  = await this.checkIntersection(element)
-            if(intersect){
-              error = true
-            }
-          }
-      }
-      if(error){
-        return response.json({"Error": `The operation registrated intersect with an other operation`})
-      }else{
-        return response.json(await this.dao.save(request.body));
+    let op_vols = request.body.operation_volumes
+    let error = false
+    if (op_vols !== undefined) {
+      for (let index = 0; index < op_vols.length; index++) {
+        const element = op_vols[index];
+        let intersect = await this.checkIntersection(element)
+        if (intersect) {
+          error = true
+        }
       }
     }
-
-    async getOperationByPoint(request: Request, response: Response, next: NextFunction) {
-      return response.json(await this.dao.getOperationByPoint(request.body))
+    if (error) {
+      return response.json({ "Error": `The operation registrated intersect with an other operation` })
+    } else {
+      return response.json(await this.dao.save(request.body));
     }
+  }
 
-    async getOperationByVolumeOperation(request: Request, response: Response, next: NextFunction) {
-      return response.json(await this.dao.getOperationByVolume(request.body))
-    }
+  async getOperationByPoint(request: Request, response: Response, next: NextFunction) {
+    return response.json(await this.dao.getOperationByPoint(request.body))
+  }
 
-    async operationsByCreator(request: Request, response: Response, next: NextFunction) {
-      // let state = request.query.state;
-      let {username,role} = response.locals.jwtPayload
-      console.log(` ------------------ operationsByCreator ${username} ------------`)
-      let ops;
-      
-      if(role == Role.PILOT){
-        ops = await this.dao.operationsByCreator(username)
-      }else{
-        ops = await this.dao.all()
-      }
-      return response.json({count:ops.length, ops});
-    }
+  async getOperationByVolumeOperation(request: Request, response: Response, next: NextFunction) {
+    return response.json(await this.dao.getOperationByVolume(request.body))
+  }
 
-    async checkIntersection(operationVolume){
-      try{
-        let operationsCount = await this.dao.getOperationVolumeByVolumeCount(operationVolume)
-        return operationsCount>0;
-      }catch(e){
-        console.log(e)
-        return true //TODO throw exception
-      }
+  // AGREGAR GET OPERATIONS BY USER, a partir del token
+  async operationsByCreator(request: Request, response: Response, next: NextFunction) {
+    // let state = request.query.state;
+    let { username, role } = response.locals.jwtPayload
+    console.log(` ------------------ operationsByCreator ${username} ------------`)
+    let ops;
+
+    // if(role == Role.PILOT){
+    ops = await this.dao.operationsByCreator(username)
+    // }else{
+    //   ops = await this.dao.all()
+    // }
+    return response.json({ count: ops.length, ops });
+  }
+
+  async checkIntersection(operationVolume) {
+    try {
+      let operationsCount = await this.dao.getOperationVolumeByVolumeCount(operationVolume)
+      return operationsCount > 0;
+    } catch (e) {
+      console.log(e)
+      return true //TODO throw exception
     }
+  }
 
 }
 
