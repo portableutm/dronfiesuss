826d9a703e81e8e18d365591f77d3fd29ed3c940
Emiliano Alonzo <emialonzo@gmail.com>
2020-01-31 14:13:02 -0300

    The get all method only can be fire by admin. The get one method if fired by pilot check the vehicle is registred by the logged user. Add validation to the save method

--- src/restControllers/VehicleController.ts
+++ src/restControllers/VehicleController.ts
@@ -1,35 +1,73 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import { VehicleDao } from "../daos/VehicleDao";
+import {  } from "../daos/UserDaos";
 import { VehicleReg } from "../entities/VehicleReg";
+import { Role } from "../entities/User";
+import { getPayloadFromResponse } from "../utils/authUtils";
+import { genericTextLenghtValidation } from "../utils/validationUtils";
 
 export class VehicleController {
 
     private dao = new VehicleDao()
-    
+
     // let x = {a: 1, b: 2, c: 3, z:26};
     // let {b, ...y} = x;
     // b ==2
     // y == {a: 1, c: 3, z:26};
 
+    //solo admin
     async all(request: Request, response: Response, next: NextFunction) {
-        let vehicles = await this.dao.all();
-        return response.json(vehicles)
+        let { role } = getPayloadFromResponse(response)
+        if (role == Role.ADMIN) {
+            let vehicles = await this.dao.all();
+            return response.json(vehicles)
+        } else {
+            return response.sendStatus(401)
+        }
     }
 
+    //solo los propios si role piloto
     async one(request: Request, response: Response, next: NextFunction) {
-        try{
-            let v = await this.dao.one(request.params.id);
-            return response.json(v)
-        }catch(error){
+        try {
+            let { role, username } = getPayloadFromResponse(response)
+            if (role == Role.ADMIN) {
+                let v = await this.dao.one(request.params.id);
+                return response.json(v)
+            } else {
+                let v = await this.dao.oneByUser(request.params.id, username);
+                return response.json(v)
+                // return response.sendStatus(401)
+            }
+
+        } catch (error) {
             return response.sendStatus(404)
         }
-        
-        
+
+
     }
 
+    //controlar datos invalidos
+    //obtener usuario del token, omitir si mandan uno por parametro
     async save(request: Request, response: Response, next: NextFunction) {
-        let v = await this.dao.save(request.body);
-        return response.json(v)
+        let v: VehicleReg = await this.dao.save(request.body);
+        v.date = undefined
+        let errors = validateVehicle(v)
+        if (errors.length == 0) {
+            let insertedVehicle = await this.dao.save(v)
+            return response.json(insertedVehicle);
+        } else {
+            response.status(400)
+            return response.json(errors)
+        }
     }
 
+}
+
+
+function validateVehicle(v: VehicleReg) {
+    let errors = []
+    if (!genericTextLenghtValidation(v.vehicleName)) {
+        errors.push("Invalid vehicle name")
+    }
+    return errors
 }
