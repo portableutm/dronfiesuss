8ef916d6b149f43ff876a658ddee9f3729a8e1cf
Emiliano Alonzo <emialonzo@gmail.com>
2020-01-31 13:08:53 -0300

    Add enum vehicleType. Add comments to entitie

--- src/entities/VehicleReg.ts
+++ src/entities/VehicleReg.ts
@@ -1,33 +1,56 @@
-import {Entity, PrimaryGeneratedColumn, Column, PrimaryColumn, ManyToOne   } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, PrimaryColumn, ManyToOne, CreateDateColumn } from "typeorm";
 import { User } from "./User";
+import { type } from "os";
+
+export enum vehicleType {
+    MULTIROTOR = "MULTIROTOR",
+    FIXEDWING = "FIXEDWING",
+    VTOL = "VTOL",
+    OTRO = "OTRO"
+}
 
 @Entity()
 export class VehicleReg {
     @PrimaryGeneratedColumn("uuid")
-    uvin? : string;
-    // @Column()
-    // 'date' : string;
-    @ManyToOne(type => User,{
+    uvin?: string;
+
+    //fecha de registro, poner automaticamente, ignorar en json
+    @CreateDateColumn({ type: "timestamp" })
+    'date'?: string;
+
+
+    @ManyToOne(type => User, {
         eager: true
     })
-    registeredBy? : User;
-    @Column({nullable:true})
-    'nNumber' : string;
-    @Column({nullable:true})
-    'faaNumber' : string;
-    @Column({nullable:true})
-    'vehicleName' : string;
-    @Column({nullable:true})
-    'manufacturer' : string;
-    @Column({nullable:true})
-    'model' : string;
-    @Column({nullable:true})
-    'class' : string;
-    @Column({nullable:true})
-    'accessType' : string;
-    @Column({nullable:true})
-    'vehicleTypeId' : string;
+    registeredBy?: User;
 
-    @Column({nullable:true})
-    'org-uuid' : string;
+    @Column({ nullable: true })
+    'nNumber': string;
+
+    @Column({ nullable: true })
+    'faaNumber'?: string;
+
+    //obligatiorio string de 1 a 255 caracteres, con trim 
+    @Column()
+    'vehicleName': string;
+
+    @Column({ nullable: true })
+    'manufacturer'?: string;
+
+    @Column({ nullable: true })
+    'model'?: string;
+
+    // example: "Multi-Rotor", definir enum: multirotor, fixedWing, vtol y otro
+    @Column()
+    'class': vehicleType;
+
+    //public private
+    @Column({ nullable: true })
+    'accessType'?: string;
+
+    @Column({ nullable: true })
+    'vehicleTypeId': string;
+
+    @Column({ nullable: true })
+    'org-uuid': string;
 }
