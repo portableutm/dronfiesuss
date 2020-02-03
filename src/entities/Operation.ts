74e4c0ca0df21c9c4b0963acb0220c938f7e7a99
Emiliano Alonzo <emialonzo@gmail.com>
2020-02-03 14:56:03 -0300

    Change some fields. Add submit_time and update_time automatically

--- src/entities/Operation.ts
+++ src/entities/Operation.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne, ManyToMany, JoinTable, OneToOne, JoinColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne, ManyToMany, JoinTable, OneToOne, JoinColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
 import { Point } from "geojson";
 
 import { OperationVolume } from "./OperationVolume";
@@ -10,8 +10,37 @@ import { PriorityElements } from "./PriorityElements";
 
 type operations_vol = Array<OperationVolume>;
 
+/**
+ * 
+    PROPOSED
+    This operation is not yet ACCEPTED. It may be awaiting information
+    from the operator, it may be in conflict with another ACCEPTED or
+    ACTIVATED operation and undergoing a negotiation process, or for
+    some other reason it is not yet able to be declared ACCEPTED.
+    ACCEPTED
+    This operation has been deemed ACCEPTED by the supporting USS. This
+    implies that the operation meets the requirements for operating in
+    the airspace based on the type of operation submitted.
+    ACTIVATED
+    This operation is active. The transition from ACCEPTED to ACTIVATED
+    is not an announced transition. The transition is implied based on
+    the submitted start time of the operation (i.e. the
+    effective_time_begin of the first OperationVolume). Note that an
+    ACTIVATED operation is not necessarily airborne, but is assumed
+    to be "using" the OperationVolumes that it has announced.
+    CLOSED
+    This operation is closed. It is not airborne and will not become
+    airborne again. If the UAS and the crew will fly again, it would
+    need to be as a new operation. A USS may announce the closure of any
+    operation, but is not required to announce unless the operation was
+    ROGUE or NONCONFORMING.
+    NONCONFORMING
+    See USS Specification for requirements to transition to this state.
+    ROGUE
+    See USS Specification for requirements to transition to this state.
+ */
 export enum OperationState {
-    PROPOSED = "PROPOSED"
+     PROPOSED = "PROPOSED"
     , ACCEPTED = "ACCEPTED"
     , ACTIVATED = "ACTIVATED"
     , CLOSED = "CLOSED"
@@ -29,37 +58,56 @@ export enum OperatonFaaRule {
 export class Operation {
     @PrimaryGeneratedColumn("uuid")
     'gufi': string;
-    // 'uss_name': string;
-    // 'discovery_reference'?: string;
-    @Column({ type: "timestamp" })
-    'submit_time': string;
-    @Column({ type: "timestamp" })
-    'update_time': string;
+
+    //agregarlos nulleables
+    @Column({ nullable: true })
+    'uss_name'?: string;
+    @Column({ nullable: true })
+    'discovery_reference'?: string;
+
+    //automaticos, ignorarlos en la entrada
+    @CreateDateColumn({ type: "timestamp" })
+    // @Column({ type: "timestamp" })
+    'submit_time'?: string;
+
+    // automaticos, ignorarlos en la entrada 
+    @UpdateDateColumn({ type: "timestamp" })
+    // @Column({ type: "timestamp" })
+    'update_time'?: string;
+
     @Column({ nullable: true })
     'aircraft_comments'?: string;
-    @Column({ nullable: true })
-    'flight_comments'?: string;
+
+    //obligatorio, despues de aplicar trim tiene que tener entre 1 y 255. 
+    //se usa como frontend como titulo
+    @Column()
+    'flight_comments': string;
+
     @Column({ nullable: true })
     'volumes_description'?: string;
 
-
     @Column({ nullable: true })
     'airspace_authorization'?: string;
+
     @Column({ nullable: true })
     'flight_number'?: string;
 
+    //agregar los comentarios sobre la nueva maquina de estados
     @Column()
     'state': OperationState //"PROPOSED" | "ACCEPTED" | "ACTIVATED" | "CLOSED" | "NONCONFORMING" | "ROGUE";
 
     @Column("geometry", { nullable: true })
     'controller_location': Point;
+
     @Column("geometry", { nullable: true })
     'gcs_location'?: Point;
 
+    //nulleable
     @Column()
     'faa_rule': OperatonFaaRule // "PART_107" | "PART_107X" | "PART_101E" | "OTHER";
 
-    // @Column(type => OperationVolume)
+
+    //tiene que tener 1 elemento
     @OneToMany(type => OperationVolume, operation_volume => operation_volume.operation, {
         eager: true,
         cascade: true
@@ -69,12 +117,14 @@ export class Operation {
 
     // 'uas_registrations': Array<UasRegistration>;
 
+    //mandar una lista de ids. controlar que existan y sean del usuario. no vacia
     @ManyToMany(type => VehicleReg
         , { eager: true }
     )
     @JoinTable()
     uas_registrations: VehicleReg[];
 
+    //se obtiene automaticamente del token
     @ManyToOne(type => User, {
         eager: false
     })
@@ -83,23 +133,28 @@ export class Operation {
     @Column({ nullable: true })
     'contact'?: string;
 
+    //no obligatorio
     @ManyToMany(type => ContingencyPlan
         , { eager: true, cascade: true },
     )
     @JoinTable()
     'contingency_plans': ContingencyPlan[];
 
+    //no obligatorio
     @ManyToMany(type => NegotiationAgreement
         , { eager: true, cascade: true }
     )
     @JoinTable()
     'negotiation_agreements'?: NegotiationAgreement[];
 
+
+    //nulleable
     @Column(type => PriorityElements)
     // @JoinColumn()
     'priority_elements'?: PriorityElements;
 
 
+    //es nuleable
     // 'metadata': EventMetadata;
 
     // @AfterLoad()
