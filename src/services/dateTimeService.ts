5bf7b55a4edef9f1083bdc7ca947ac726de07002
Emiliano Alonzo <emialonzo@gmail.com>
2020-02-04 18:12:42 -0300

    Add fake dateTime to test operation state change

--- src/services/dateTimeService.ts
+++ src/services/dateTimeService.ts
@@ -1,12 +1,16 @@
 
 
-let fakeDate = new Date()
+let fakeDate = new Date("2019-12-11T13:59:10.000Z")
 
 export function getNow() {
     // return Date.now();
-    return new Date();
+
+    // return new Date();
+
+    return fakeDate
 }
 
-export function fakeTime(){
-
+export function fakeTime(newDate:string) : Date{
+     fakeDate = new Date(newDate)
+     return fakeDate
 }
