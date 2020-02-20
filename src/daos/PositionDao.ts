3b512685eec9390f5d7992a86222ef3dc9663b79
Emiliano Alonzo <emialonzo@gmail.com>
2020-02-20 15:11:35 -0300

    Add checkPostion to verify that the position is contained on operation polygon

--- src/daos/PositionDao.ts
+++ src/daos/PositionDao.ts
@@ -1,5 +1,6 @@
 import {getRepository} from "typeorm";
 import { Position } from "../entities/Position";
+import { Operation } from "../entities/Operation";
 
 export class  PositionDao{
 
@@ -15,6 +16,24 @@ export class  PositionDao{
 
     async save(entity:Position) {
         return this.repository.save(entity);
+    }
+
+    async checkPositionWithOperation(position : Position){
+        console.log("")
+        const result = 
+        await getRepository(Operation)
+        .createQueryBuilder("operation")
+        .select("st_contains(operation_volume.\"operation_geography\" ,ST_GeomFromGeoJSON(:origin))", "inOperation")
+        .innerJoin("operation.operation_volumes", "operation_volume")
+        .where("operation.\"gufi\" = :gufi")
+        .setParameters({
+            gufi: position.gufi,
+            origin: JSON.stringify(position.location)
+        })
+        .getRawOne();
+
+        console.log(result)
+        return result
     }
 
     // async remove(id : string) {
