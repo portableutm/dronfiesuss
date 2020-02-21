5387a6cc8d5eeac3d331f140a06c6b4487e55008
Emiliano Alonzo <emialonzo@gmail.com>
2020-02-21 13:59:01 -0300

    Add uvr intersection metheod

--- src/daos/UASVolumeReservationDao.ts
+++ src/daos/UASVolumeReservationDao.ts
@@ -1,5 +1,6 @@
 import {getRepository} from "typeorm";
 import { UASVolumeReservation } from "../entities/UASVolumeReservation";
+import { OperationVolume } from "../entities/OperationVolume";
 
 export class UASVolumeReservationDao {
 
@@ -24,5 +25,25 @@ export class UASVolumeReservationDao {
         let entitie = await this.repository.findOne(id);
         await this.repository.remove(entitie);
     }
+
+
+    async countUvrIntersections(volume : OperationVolume){
+        return this.repository
+        .createQueryBuilder("uas_volume_reservation")
+        .where("(tsrange(effective_time_begin, \"effective_time_end\") && tsrange(:date_begin, :date_end) ) "
+        + " AND (numrange(\"min_altitude\", \"max_altitude\") && numrange(:min_altitude, :max_altitude)) " 
+        + " AND (ST_Intersects(\"geography\" ,ST_GeomFromGeoJSON(:geom)))"
+        )
+        .setParameters({
+            // gufi: gufi,
+            date_begin : volume.effective_time_begin,
+            date_end : volume.effective_time_end,
+            min_altitude : volume.min_altitude,
+            max_altitude : volume.max_altitude,
+            geom: JSON.stringify(volume.operation_geography)
+        })
+        .getCount()
+    }
+
 
 }
