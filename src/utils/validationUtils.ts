431bc80f340e47e1d71b17378b5ad210a7b624e3
Emiliano Alonzo <emialonzo@gmail.com>
2020-03-20 13:52:00 -0300

    add validation date time

--- src/utils/validationUtils.ts
+++ src/utils/validationUtils.ts
@@ -6,7 +6,7 @@
  * @param text text to validate
  */
 export function genericTextLenghtValidation(text: String) {
-    return notUndefined(text) && (text.length > 1) && (text.length<256)
+    return notUndefined(text) && (text.length > 1) && (text.length < 256)
 }
 
 /**
@@ -15,6 +15,15 @@ export function genericTextLenghtValidation(text: String) {
 export function notUndefined(text: String): boolean {
     return ((text !== undefined) && (text !== null));
 }
+
+
+export function validateStringDateIso(strDateIso: string) {
+    let isoDateRegex = /^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d(\.\d+)?([+-][0-2]\d:[0-5]\d|Z)$/
+    let strValid = isoDateRegex.test(strDateIso)
+    return strValid
+}
+
+export const dateTimeStringFormat = "yyyy-mm-ddThh:mm:ss.mmmZ"
 
 
 
