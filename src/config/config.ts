69f77b16fb9f0d8f619d0c549576c6ba186fa91a
Emiliano Alonzo <emialonzo@gmail.com>
2020-03-27 16:26:17 -0300

    Update config.ts to use env vars. Check if in production that change the jwt secret.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,8 @@
-export const jwtSecret: string =  "@876fyivd&(&*%EH";
-export const jwtExpTime: string =  "1h";
+export const jwtSecret: string =  process.env.JWTSECRET;//"@876fyivd&(&*%EH";
+export const jwtExpTime: string =  process.env.JWT_EXPIRATION_TIME; //"1h";
+
+if ((process.env.NODE_ENV=="production")  && (process.env.JWTSECRET=='' || process.env.JWTSECRET == "changeMe")){
+    throw "You must set a valid JWTSECRET on .env file"
+} 
 
 
