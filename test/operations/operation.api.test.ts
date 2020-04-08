9d683f24aec3a34207a3e93402fb6261df180d89
Emiliano Alonzo <emialonzo@gmail.com>
2020-04-08 13:08:44 -0300

    Re write operation test to work correctly

--- test/operations/operation.api.test.ts
+++ test/operations/operation.api.test.ts
@@ -6,18 +6,139 @@ chai.use(chaiHttp);
 chai.should();
 import { VehicleDao } from "../../src/daos/VehicleDao";
 
+import { getToken } from "../../src/services/tokenService";
+import { Users } from "../../src/data/users_data";
+
 import { app, init, initAsync } from "../../src/index";
+import { Role } from "../../src/entities/User";
 
-describe.skip(' >>> Operation test <<< ', function () {
+describe(' >>> Operation test <<< ', function () {
 
-    before(async () => {
-        await initAsync()
+    // this.timeout(3000);
+    before(function (done) {
+        this.timeout(3000);
+
+        initAsync()
+            .then(done)
+            .catch(done)
     })
-    it("Should get all operations")
-    it("Should get all operations by creator")
-    it("Should get a operation")
-    it("Should create a new operation")
 
+    it("GET /operation Should get all operations", function (done) {
+        // it("Should get all users", (done) => {
+        chai.request(app.app)
+            .get('/operation')
+            .set('bypass', 'a')
+            .set('Accept', 'application/json')
+            // .expect('Content-Type', /json/)
+            .then(function (res) {
+                res.should.have.status(200);
+                res.body.ops.length.should.be.eq(4)
+                done();
+            })
+            .catch(done)
+
+    });
+
+
+    it("Should not get all operations if user role is not ADMIN", function (done) {
+        // let user = Users[2]
+        // let token = getToken(user.email, user.username, user.role)
+        let token = getToken('maurine@dronfies.com', 'MaurineFowlie', Role.PILOT)
+
+        chai.request(app.app)
+            .get('/operation')
+            // .set('bypass', 'a')
+            .set('auth', token)
+            .set('Accept', 'application/json')
+            .then(function (res) {
+                res.should.have.status(401);
+                // res.body.length.should.be.eq(4)
+                done();
+            })
+            .catch(done)
+
+    });
+
+    it("Should get 2 operations by creator MaurineFowlie", function (done) {
+        // let user = Users[2]
+        let token = getToken('maurine@dronfies.com', 'MaurineFowlie', Role.PILOT)
+        // let token = getToken(user.email, user.username, user.role)
+        chai.request(app.app)
+            .get('/operation/creator')
+            // .set('bypass', 'a')
+            .set('auth', token)
+            .set('Accept', 'application/json')
+            // .expect('Content-Type', /json/)
+            .then(function (res) {
+                res.should.have.status(200);
+                res.body.ops.length.should.be.eq(2)
+                done();
+            })
+            .catch(done)
+    });
+
+    it("Should get 0 operations by creator TrulaRemon", function (done) {
+        let token = getToken('trula@dronfies.com', 'TrulaRemon', Role.PILOT)
+        chai.request(app.app)
+            .get('/operation/creator')
+            .set('auth', token)
+            .set('Accept', 'application/json')
+            .then(function (res) {
+                res.should.have.status(200);
+                res.body.ops.length.should.be.eq(0)
+                done();
+            })
+            .catch(done)
+    });
+
+    it("Should get an operation of MaurineFowlie with MaurineFowlie user", function (done) {
+        let token = getToken('maurine@dronfies.com', 'MaurineFowlie', Role.PILOT)
+        let gufi = 'b92c7431-13c4-4c6c-9b4a-1c3c8eec8c63'
+        chai.request(app.app)
+            .get(`/operation/${gufi}`)
+            .set('auth', token)
+            .set('Accept', 'application/json')
+            .then(function (res) {
+                res.should.have.status(200);
+                done();
+            })
+            .catch(done)
+    });
+
+    it("Should not get an operation of MaurineFowlie with TrulaRemon user", function (done) {
+        let token = getToken('trula@dronfies.com', 'TrulaRemon', Role.PILOT)
+        let gufi = 'b92c7431-13c4-4c6c-9b4a-1c3c8eec8c63'
+        chai.request(app.app)
+            .get(`/operation/${gufi}`)
+            .set('auth', token)
+            .set('Accept', 'application/json')
+            .then(function (res) {
+                res.should.have.status(404);
+                done();
+            })
+            .catch(done)
+    });
+
+    // it("Should create a new operation", function(done){
+    //     // it("Should get all users", (done) => {
+    //     chai.request(app.app)
+    //         .get('/user')
+    //         .set('bypass', 'a')
+    //         .set('Accept', 'application/json')
+    //         .expect('Content-Type', /json/)
+    //         .then(function (res) {
+    //             res.body.length.should.be.eq(11)
+    //         })
+    //         .catch(done)
+    //         .expect(200, done)
+    // });
+
+
+    // ***************************************
+    // ***************************************
+    // ***************************************
+    // ***************************************
+    // ***************************************
 
     // it("should get 403 when fetch protected resource", (done) => {
     //     chai.request(app.app)
