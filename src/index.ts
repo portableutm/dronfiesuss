cea8e54cf16e3d63dd9d42c0ff89efb05ce81107
Emiliano Alonzo <emialonzo@gmail.com>
2020-04-08 13:10:02 -0300

    Doc file and don't start web server for test env

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,20 @@ export let app : App;
 
 if(process.env.NODE_ENV == "dev"){
     console.log("DEV env")
+    //if passed a port on process.env.PORT, will overwrite the port passed to app constructor
     app = new App(controllers, 3000, "dev");
     app.listen();
     
 } else if(process.env.NODE_ENV == "cloud"){
     console.log("cloud env")
+    //if passed a port on process.env.PORT, will overwrite the port passed to app constructor
+    app = new App(controllers, 3000, "cloud");
+    app.listen();
+    
+}
+else if(process.env.NODE_ENV == "production"){
+    console.log("cloud env")
+    //if passed a port on process.env.PORT, will overwrite the port passed to app constructor
     app = new App(controllers, 3000, "cloud");
     app.listen();
     
@@ -17,6 +26,7 @@ if(process.env.NODE_ENV == "dev"){
 export function init(){
     console.log("Test env")
     if(app == undefined){
+        //if passed a port on process.env.PORT, will overwrite the port passed to app constructor
         app = new App(controllers, 3000, "test");
         app.listen();
         app.printStatus();
@@ -27,10 +37,12 @@ export async function initAsync(){
     return new Promise(resolve => {
         if(app == undefined){
             app = new App(controllers, 3000, "test", ()=>{
-                app.listen(()=>{
-                    app.printStatus();
-                    resolve();
-                });
+                // app.listen(()=>{
+                //     app.printStatus();
+                //     resolve();
+                // });
+                resolve();
+
             });
         }else{
             resolve();
