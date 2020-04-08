4ebd7f14ff59ea2ac84e464f3e4c9719f400f070
Emiliano Alonzo <emialonzo@gmail.com>
2020-04-08 14:14:34 -0300

    Add user test to cover 100% of UserController

--- test/user/user.api.test.ts
+++ test/user/user.api.test.ts
@@ -43,6 +43,15 @@ describe('>>> User rest controller test <<<', function () {
             .expect(401, done)
     });
 
+    it("GET /user Should not get all users for non ADMIN user",  function (done) {
+        let token = getToken('maurine@dronfies.com', 'MaurineFowlie', Role.PILOT)
+        request(app.app)
+            .get('/user')
+            .set('Accept', 'application/json')
+            .set('auth', token)
+            .expect(401, done)
+    });
+
     it("GET /user Should get all users",  function (done) {
         chai.request(app.app)
             .get('/user')
@@ -86,6 +95,52 @@ describe('>>> User rest controller test <<<', function () {
                 .then(res => {
                     dao.all().then(function (newUsers) {
                         assert.equal(newUsers.length, CountPreInsert+1)
+                        done();
+                    }).catch(err => {
+                        console.error(err);
+                        done(err);
+                    });
+
+                })
+                .catch(err => {
+                    console.error(err);
+                    done(err);
+                });
+
+        })
+    });
+
+    it("POST /user Should not create a new user with a non ADMIN user", function (done) {
+        let dao = new UserDao()
+        dao.all().then(function (users) {
+            let CountPreInsert = users.length
+            let user: User = {
+                username: "AnOtherUserToInsert",
+                email: `anotherusertoinsert@dronfies.com`,
+                firstName: `Algun`,
+                lastName: `Nombre`,
+                password: `password`,
+                role: Role.ADMIN
+            }
+            let token = getToken('maurine@dronfies.com', 'MaurineFowlie', Role.PILOT)
+
+            request(app.app)
+                .post('/user')
+                .set('auth', token)
+                .set('Accept', 'application/json')
+                // .expect('Content-Type', /json/)
+                .send(user)
+                // .expect(function (res) {
+                    // res.body.should.have.property('username').equal(user.username);
+                    // res.body.should.have.property('email').equal(user.email);
+                    // res.body.should.have.property('firstName').equal(user.firstName);
+                    // res.body.should.have.property('lastName').equal(user.lastName);
+                    // res.body.should.have.property('role').equal(user.role);
+                // })
+                .then(res => {
+                    res.should.have.status(401);
+                    dao.all().then(function (newUsers) {
+                        assert.equal(newUsers.length, CountPreInsert)
                         done();
                     }).catch(err => {
                         console.error(err);
@@ -173,6 +228,127 @@ describe('>>> User rest controller test <<<', function () {
                     done(err);
                 });
         })
+    });
+
+    let invalidUsers = [
+        {
+            username: "wakawaka",
+            email: `admindronfies.com`,
+            firstName: `NewName`,
+            lastName: `NewLastName`,
+            password: `password`,
+            role: Role.PILOT
+        },
+        {
+            username: "",
+            email: `admin@dronfies.com`,
+            firstName: `NewName`,
+            lastName: `NewLastName`,
+            password: `password`,
+            role: Role.PILOT
+        },
+        {
+            username: "wakawaka",
+            email: `admin@dronfies.com`,
+            firstName: ``,
+            lastName: `NewLastName`,
+            password: `password`,
+            role: Role.PILOT
+        },{
+            username: "wakawaka",
+            email: `admin@dronfies.com`,
+            firstName: `NewName`,
+            lastName: ``,
+            password: `password`,
+            role: Role.PILOT
+        },
+        {
+            username: "wakawaka",
+            email: `admin@dronfies.com`,
+            firstName: ``,
+            lastName: ``,
+            password: `password`,
+            role: Role.PILOT
+        },
+        {
+            username: "wakawaka",
+            email: `admindronfies.com`,
+            firstName: ``,
+            lastName: ``,
+            password: `password`,
+            role: Role.PILOT
+        },
+        {
+            username: "wakawaka",
+            email: `admindronfies.com`,
+            // firstName: ``,
+            lastName: ``,
+            password: `password`,
+            role: Role.PILOT
+        },
+        {
+            username: "wakawaka",
+            email: `admindronfies.com`,
+            firstName: ``,
+            // lastName: ``,
+            password: `password`,
+            role: Role.PILOT
+        },
+        {
+            username: "wakawaka",
+            email: `admindronfies.com`,
+            firstName: ``,
+            lastName: ``,
+            password: ``,
+            role: Role.PILOT
+        },
+        {
+            username: "wakawaka",
+            email: `admindronfies.com`,
+            firstName: `12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890`,
+            lastName: ``,
+            password: `password`,
+            role: Role.PILOT
+        }
+    ]
+
+    invalidUsers.forEach(user => {
+        it("POST /user Should not create a new user with invalid fields", function (done) {
+            let dao = new UserDao()
+            dao.all().then(function (users) {
+                let CountPreInsert = users.length
+                // let user: User = {
+                //     username: "wakawaka",
+                //     email: `admin@dronfies.com`,
+                //     firstName: `NewName`,
+                //     lastName: `NewLastName`,
+                //     password: `password`,
+                //     role: Role.PILOT
+                // }
+    
+                request(app.app)
+                    .post('/user')
+                    .set('bypass', 'a')
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /json/)
+                    .send(user)
+                    .then(res => {
+                        res.should.have.status(400);
+                        dao.all().then(function (newUsers) {
+                            assert.equal(newUsers.length, CountPreInsert)
+                            done();
+                        }).catch(err => {
+                            console.error(err);
+                            done(err);
+                        });
+    
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        done(err);
+                    });
+            })
+        });
     });
 
     it("GET /user/{username} Should get a users", function (done) {
