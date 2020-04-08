e255241571785e0a9a93da4e4182f882110c686f
Emiliano Alonzo <emialonzo@gmail.com>
2020-04-08 16:21:54 -0300

    Not use socket io on testing

--- src/services/asyncBrowserComunication.ts
+++ src/services/asyncBrowserComunication.ts
@@ -3,21 +3,33 @@ import { Position } from "../entities/Position";
 import { User } from "../entities/User";
 
 
+function send(topic, object){
+    if(process.env.NODE_ENV != "test"){
+        app.io.emit(topic, object)
+    } else {
+        return [topic, object]
+    }
+
+}
 
 export function sendPositionToMonitor(position ){
-    app.io.emit('new-position', position)
+    // app.io.emit('new-position', position)
+    return send('new-position', position)
 }
 
 export function sendOperationFlyStatus(inOperation ){
-    app.io.emit('position-status', inOperation)
+    // app.io.emit('position-status', inOperation)
+    return send('position-status', inOperation)
 }
 
 
 export function sendAlgo(position : Position){
-    app.io.emit('new-position', position)
+    // app.io.emit('new-position', position)
+    return send('new-position', position)
 }
 
 
 export function sendUserLogged(user : User){
-    app.io.emit('user-logged')
+    // app.io.emit('user-logged')
+    return send('user-logged', {})
 }
