81f0ae76e70fa6673005923604c011fcf1d1545e
Emiliano Alonzo <emialonzo@gmail.com>
2020-04-17 13:21:30 -0300

    Fixed id for UVRs

--- src/data/uasVolumeReservation_data.ts
+++ src/data/uasVolumeReservation_data.ts
@@ -5,6 +5,7 @@ import { deepCopy } from "../utils/entitiesUtils";
 export let uasVolumeReservationList = []
 
 let uasVolumeReservation = new UASVolumeReservation()
+uasVolumeReservation.message_id = "0a8c53a7-4300-472d-844b-c0cfed9f1b17",
 uasVolumeReservation.geography = { "type": "Polygon", "coordinates": [ [ [ -56.163482666015625, -34.910710205494524 ], [ -56.17043495178223, -34.91324402708751 ], [ -56.17009162902832, -34.91500357940347 ], [ -56.16305351257324, -34.914581290287316 ], [ -56.163482666015625, -34.910710205494524 ] ] ]}
 uasVolumeReservation.cause = UASVolumeReservationCause.MUNICIPALITY
 uasVolumeReservation.type = UASVolumeReservationType.DYNAMIC_RESTRICTION
@@ -22,6 +23,7 @@ uasVolumeReservation.required_support.push(UASVolumeReservationRequiredSupport.E
 uasVolumeReservationList.push(deepCopy(uasVolumeReservation))
 
 uasVolumeReservation.geography = {"type": "Polygon","coordinates": [[[-56.16416931152344,-34.901841214080186],[-56.16777420043943,-34.90592388467209],[-56.167430877685526,-34.907683593878055],[-56.1635684967041,-34.909724809310575],[-56.16416931152344,-34.901841214080186]]]}
+uasVolumeReservation.message_id = "6919c2b6-5350-4bff-97e3-9b460d772783"
 uasVolumeReservation.cause = UASVolumeReservationCause.SECURITY
 uasVolumeReservation.permitted_uas.push( UASVolumeReservationPermitedUas.RADIO_LINE_OF_SIGHT)
 uasVolumeReservation.type = UASVolumeReservationType.STATIC_ADVISORY
