b8462fec964abda0d3fa0af52ee4bef80be9d02f
Emiliano Alonzo <emialonzo@gmail.com>
2020-04-29 12:39:47 -0300

    Add a fake time to test

--- src/services/dateTimeService.ts
+++ src/services/dateTimeService.ts
@@ -1,11 +1,23 @@
 let fakeDate = new Date("2019-12-11T13:59:10.000Z")
 
-export function getNow() {
-
-    return new Date();
-}
+// export function getNow() {
+//     return new Date();
+// }
 
 export function fakeTime(newDate:string) : Date{
      fakeDate = new Date(newDate)
      return fakeDate
+}
+
+
+export let getNow;
+
+if (process.env.NODE_ENV != "test"){
+    getNow = function(){
+        return new Date();
+    }
+}else {
+    getNow = function(){
+        return fakeDate
+    }
 }
