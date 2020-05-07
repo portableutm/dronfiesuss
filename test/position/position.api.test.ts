deba1eab6c382361b3780fd22acba78d45c80cfd
Emiliano Alonzo <emialonzo@gmail.com>
2020-05-07 12:22:41 -0300

    Update position tests

--- test/position/position.api.test.ts
+++ test/position/position.api.test.ts
@@ -9,7 +9,11 @@ import { app, initAsync } from "../../src/index";
 
 import { getToken } from "../../src/services/tokenService";
 import { Role } from "../../src/entities/User";
-import { TEST_TIMEOUT } from "../conf"; 
+import { TEST_TIMEOUT } from "../conf";
+import { deepCopy } from "../../src/utils/entitiesUtils";
+import { Operations } from "../../src/data/operations_data";
+import { OperationState } from "../../src/entities/Operation";
+import { OperationDao } from "../../src/daos/OperationDaos";
 
 describe('>>> Position entity <<< ', function () {
 
@@ -33,6 +37,85 @@ describe('>>> Position entity <<< ', function () {
             .catch(done);
     });
 
+    describe("Check state change", function () {
+        it("should insert a new position in operation", function (done) {
+            let token = getToken('admin@dronfies.com', 'admin', Role.ADMIN)
+            let gufi = "c42e9384-14d2-9b6b-1c1c-1c3c8aaa2b99"
+            let opDao = new OperationDao()
+            let op = deepCopy(Operations[0])
+            op.gufi = gufi
+            op.uas_registrations = []
+            op.flight_comments = "For automate Testing operation and position"
+            op.state = OperationState.ACTIVATED
+            op.operation_volumes[0].operation_geography = { "type": "Polygon", "coordinates": [[[-56.152325, -34.875792], [-56.152411, -34.881847], [-56.148033, -34.879383], [-56.152325, -34.875792]]] }
+            op.operation_volumes[0].min_altitude = 0
+            op.operation_volumes[0].max_altitude = 90
+
+            opDao.save(op).then(function (op) {
+                let positionToInsert = {
+                    "altitude_gps": 30,
+                    "location": {
+                        "type": "Point",
+                        "coordinates": [
+                            -56.15069389343262,
+                            -34.87936529506962
+                        ]
+                    },
+                    "time_sent": "2019-12-11T19:59:10.000Z",
+                    "gufi": gufi,
+                    "heading": 160
+                }
+                chai.request(app.app)
+                    .post('/position')
+                    .set('auth', token)
+                    .send(positionToInsert)
+                    .then(function (res) {
+                        res.should.have.status(200);
+                        res.body.should.have.property('id');
+                        opDao.one(gufi).then(function (op: any) {
+                            op.state.should.eq(OperationState.ACTIVATED)
+                            done();
+                        }).catch(done)
+                    })
+                    .catch(done);
+            }).catch(done)
+        });
+
+        it("should insert a new position outside operation and change the state to rouge", function (done) {
+            let token = getToken('admin@dronfies.com', 'admin', Role.ADMIN)
+            let gufi = "c42e9384-14d2-9b6b-1c1c-1c3c8aaa2b99"
+            let positionToInsert = {
+                "altitude_gps": 30,
+                "location": {
+                    "type": "Point",
+                    "coordinates": [
+                        -56.15522146224975,
+                        -34.87933008912137
+                    ]
+                },
+                "time_sent": "2019-12-11T19:59:10.000Z",
+                "gufi": gufi,
+                "heading": 160
+            }
+            chai.request(app.app)
+                .post('/position')
+                .set('auth', token)
+                .send(positionToInsert)
+                .then(function (res) {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id');
+                    let opDao = new OperationDao()
+
+                    opDao.one(gufi).then(function (op: any) {
+                        op.state.should.eq(OperationState.ROGUE)
+                        done();
+                    }).catch(done)
+                })
+                .catch(done);
+        });
+    })
+
+
 
     //TODO check operation change it status
     it("should insert a new position outside operation", function (done) {
@@ -48,21 +131,16 @@ describe('>>> Position entity <<< ', function () {
                 ]
             },
             "time_sent": "2019-12-11T19:59:10.000Z",
-            "gufi" : "f7891e78-9bb4-431d-94d3-1a506910c254",
-            "heading" : 160
+            "gufi": "f7891e78-9bb4-431d-94d3-1a506910c254",
+            "heading": 160
         }
         chai.request(app.app)
             .post('/position')
-            .set('bypass', 'a')
+            .set('auth', token)
             .send(positionToInsert)
             .then(function (res) {
                 res.should.have.status(200);
                 res.body.should.have.property('id');
-                // dao.all().then(function(vehicles){
-                //     // vehicles.length.should.be.eq(vehicleCountPreInsert+1)
-                //     done();
-                // })
-                // .catch(done)
                 done();
             })
             .catch(done);
@@ -70,32 +148,27 @@ describe('>>> Position entity <<< ', function () {
 
     it("should insert a new position in operation", function (done) {
         let token = getToken('admin@dronfies.com', 'admin', Role.ADMIN)
-        let dao = new PositionDao()
+        // let dao = new PositionDao()
         let positionToInsert = {
             "altitude_gps": 30,
             "location": {
                 "type": "Point",
                 "coordinates": [
                     -56.15389108657837,
-          -34.90865141786639
+                    -34.90865141786639
                 ]
             },
             "time_sent": "2019-12-11T19:59:10.000Z",
-            "gufi" : "f7891e78-9bb4-431d-94d3-1a506910c254",
-            "heading" : 160
+            "gufi": "f7891e78-9bb4-431d-94d3-1a506910c254",
+            "heading": 160
         }
         chai.request(app.app)
             .post('/position')
-            .set('bypass', 'a')
+            .set('auth', token)
             .send(positionToInsert)
             .then(function (res) {
                 res.should.have.status(200);
                 res.body.should.have.property('id');
-                // dao.all().then(function(vehicles){
-                //     // vehicles.length.should.be.eq(vehicleCountPreInsert+1)
-                //     done();
-                // })
-                // .catch(done)
                 done();
             })
             .catch(done);
@@ -114,27 +187,21 @@ describe('>>> Position entity <<< ', function () {
                 ]
             },
             "time_sent": "2019-12-11T19:59:10.000Z",
-            "gufi" : "f7891e78-9bb4-431d-94d3-1a506910c254",
-            "heading" : 200
+            "gufi": "f7891e78-9bb4-431d-94d3-1a506910c254",
+            "heading": 200
         }
         chai.request(app.app)
             .post('/position')
-            .set('bypass', 'a')
+            .set('auth', token)
             .send(positionToInsert)
             .then(function (res) {
                 res.should.have.status(400);
-                // res.body.should.have.property('id');
-                // dao.all().then(function(vehicles){
-                //     // vehicles.length.should.be.eq(vehicleCountPreInsert+1)
-                //     done();
-                // })
-                // .catch(done)
                 done();
             })
             .catch(done);
     });
-    
-    
+
+
 
     it("should get a position", function (done) {
         let id = "1";
@@ -143,41 +210,12 @@ describe('>>> Position entity <<< ', function () {
             .set('bypass', 'a')
             .then(function (res) {
                 res.should.have.status(200);
-                // res.body.should.have.property('uvin');
-                // res.body.should.have.property('vehicleName').equal("vehicle_name9");
                 done();
             })
             .catch(done)
     });
 
-    // it("should not get a vehicle for anonymous user", function (done) {
-    //     let uvin = "bd9b2eb6-7ab7-442e-b99c-78890581f198";
-    //     chai.request(app.app)
-    //         .get(`/vehicle/${uvin}`)
-    //         // .set('bypass', 'a')
-    //         .then(function (res) {
-    //             res.should.have.status(401);
-    //             // res.body.should.have.property('uvin');
-    //             // res.body.should.have.property('vehicleName').equal("vehicle_name9");
-    //             done();
-    //         })
-    //         .catch(done)
-    // });
 
-    // it("GET /vehicle/${cualquierVehiiculo} should not get a vehicle", function (done) {
-    //     let uvin = "bd9c2ea6-7ab7-442e-b99c-78890181c198";
-    //     chai.request(app.app)
-    //         .get(`/vehicle/${uvin}`)
-    //         .set('bypass', 'a')
-    //         .then(function (res) {
-    //             res.should.have.status(404);
-    //             // res.body.should.have.property('uvin');
-    //             // res.body.should.have.property('vehicleName').equal("vehicle_name9");
-    //             done();
-    //         })
-    //         .catch(done)
-
-    // });
 
 
 });
