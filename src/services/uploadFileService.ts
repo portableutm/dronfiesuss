e7f11d98de2917901a04ec386953777485493e93
Emiliano Alonzo <emialonzo@gmail.com>
2020-11-12 16:21:07 -0300

    Update upload services

--- src/services/uploadFileService.ts
+++ src/services/uploadFileService.ts
@@ -3,9 +3,9 @@ import { url } from "inspector";
 import { backendUrl, uploadFolder } from "../config/config";
 
 
-const uploadPath = '/Users/dronfies/desarrollo/utm/dronfiesuss/' + uploadFolder
+const uploadPath = '/Users/dronfies/desarrollo/utm/dronfiesuss/src' + uploadFolder
 
-export const getFileUrl = (filename:string) => `${backendUrl}/${uploadFolder}/${filename}`
+export const getUrl = (filename:string) => `${backendUrl}/${uploadFolder}/${filename}`
 
 
 // File: {"fieldname":"file","originalname":"prueba.txt","encoding":"7bit","mimetype":"text/plain"}
